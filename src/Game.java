2cee7f7594affdf3454e6c1ba6f7f5c0a5d20814
TobinPalmer <tobinkpalmer@gmail.com>
2023-12-14 22:14:45 -0800

    Using ScheduledExecutorService instead of while loop is less hacky

--- src/Game.java
+++ src/Game.java
@@ -1,26 +1,25 @@
 import Render.Renderable;
 import Render.Renderer;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Game {
     public static void main(String[] args) {
         Controller controller = new Controller();
         new Thread(controller).start();
         Bird bird = new Bird();
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-        while (true) {
+        executorService.scheduleAtFixedRate(() -> {
             if (controller.getJump()) {
                 bird.jump();
             }
-            
+
             bird.update();
 
             Renderer.render(new Renderable[]{bird});
-
-            try {
-                Thread.sleep(16);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        }, 0, 16, TimeUnit.MILLISECONDS);
     }
 }
